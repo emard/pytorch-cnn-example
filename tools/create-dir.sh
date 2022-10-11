dc9a6e1ca69467c4e318288ba4e03ba7e53c696b
Emard <vordah@gmail.com>
2022-10-11 10:03:14 +0200

    tools script to generate dir structore and symlink to test image

--- tools/create-dir.sh
+++ tools/create-dir.sh
@@ -4,9 +4,10 @@ mkdir -p generated/saved_models
 mkdir -p generated/Image
 mkdir -p generated/Semantic/1_stone
 mkdir -p generated/Semantic/2_void
+ln -sf generated/Image/image000.jpg test-gen.jpg
 cd generated/Semantic/
-ln -s 1_stone 1
-ln -s 2_void 2
+ln -sf 1_stone 1
+ln -sf 2_void 2
 
 
 
