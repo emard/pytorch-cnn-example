0944316ef6bdd994cbfc8ac92a0139f9a4c0c040
Emard <vordah@gmail.com>
2022-10-11 10:23:00 +0200

    install scripts

---
+++ tools/install-root.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+apt install python3-pil python3-matplotlib python3-opencv python3-typing-extensions
+apt install nvidia-driver
--- tools/install-user.sh
+++ tools/install-user.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-apt install python3-pil python3-matplotlib python3-opencv python3-typing-extensions
-apt install nvidia-driver
-
 # for NVIDIA 3090 and CUDA 11.5.x
 pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
