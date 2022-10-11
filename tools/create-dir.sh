457265520b196b9eee576d6715b3360398c69e0e
Emard <vordah@gmail.com>
2022-10-11 09:34:56 +0200

    fix create dir

--- tools/create-dir.sh
+++ tools/create-dir.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-mkdir -p generated/saved/models
-mkdir -p generated/saved/Image
-mkdir -p generated/saved/Semantic/1_stone
-mkdir -p generated/saved/Semantic/2_void
-cd generated/saved/Semantic/
+mkdir -p generated/saved_models
+mkdir -p generated/Image
+mkdir -p generated/Semantic/1_stone
+mkdir -p generated/Semantic/2_void
+cd generated/Semantic/
 ln -s 1_stone 1
 ln -s 2_void 2
 
