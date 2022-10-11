2f49f632f949aeae083fa7bb232c0b27d5cceea1
Emard <vordah@gmail.com>
2022-10-11 10:20:52 +0200

    install script

--- tools/cudainstall.sh
+++ tools/cudainstall.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-# for NVIDIA 3090 and CUDA 11.5.x
+apt install python3-pil python3-matplotlib python3-opencv python3-typing-extensions
+apt install nvidia-driver
 
+# for NVIDIA 3090 and CUDA 11.5.x
 pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
