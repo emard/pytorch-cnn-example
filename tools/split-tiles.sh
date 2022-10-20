a57b2795e186c3fb9da88bfc36e74782dc510eb3
Emard <vordah@gmail.com>
2022-10-20 23:47:35 +0200

    center crop to allow splitting to full tiles

--- tools/split-tiles.sh
+++ tools/split-tiles.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
-# 0 as x or y describes full size
+
+# center split to get only full tiles of wanted size
+# discard border tiles smaller than wanted size
 
 # in case of resources exhausted error:
 # find / -name "policy.xml"
+# /etc/ImageMagick-6/policy.xml
 # <policy domain="resource" name="disk" value="8GiB"/>
+
+# wanted tile size
+tile_x=900
+tile_y=900
 
 path=/tmp/airvoid
 
 rm -rf ${path}
-mkdir ${path}
-convert $1 -crop 900x900 \
--set filename:row "%[fx:page.y/900+100]" \
--set filename:col "%[fx:page.x/900+100]" \
+mkdir  ${path}
+
+# calculate crop size that fits splitting to full tiles
+crop_size=$(identify -format "%[fx:(w - w % ${tile_x})]x%[fx:(h - h % ${tile_y})]" $1)
+# crop center part of image and split to tiles
+# +100 in filename fixes alphabetic sort, similar to leading zeros
+convert $1 +repage \
+-gravity center -crop ${crop_size}+0+0 +gravity +repage \
+-crop ${tile_x}x${tile_y} \
+-set filename:row "%[fx:page.y/${tile_x}+100]" \
+-set filename:col "%[fx:page.x/${tile_y}+100]" \
++repage \
  "${path}/tile_%[filename:row]_%[filename:col].png"
