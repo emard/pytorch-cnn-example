1e521377f3dafc31a79314aebabe6b10cc31ab76
Emard <vordah@gmail.com>
2022-11-26 21:26:12 +0100

    install script updated for torch 1.13

--- tools/install-user.sh
+++ tools/install-user.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
+# https://pytorch.org/get-started/locally
+
 # for NVIDIA 3090 and CUDA 11.5.x
-pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
+# pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
+
+# for NVIDIA 3090 and CUDA 11.6
+pip install torch torchvision
