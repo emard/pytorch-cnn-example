27ecdda2fa7020fe94238f822471ff3714b0d3a0
Emard <vordah@gmail.com>
2023-12-01 01:51:18 +0100

    mark.sh batch (parameter list of files)

--- mark.sh
+++ mark.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-filename=$1
-outfile=$2
+
+for filename in $*
+do
 filename_without_extension="${filename%.*}"
 extension="${filename##*.}"
 # echo $filenoext $extension
 ../en480img/en480img.py $filename
 sh /tmp/en480marker.sh $filename ${filename_without_extension}-mark.$extension
 echo Output: ${filename_without_extension}-mark.$extension
+done;
