ebba901b690ada611ed5b172ff419b9d912e213d
Emard <vordah@gmail.com>
2023-12-20 00:14:24 +0100

    infer and mark shell scripts commands to copy ImageNumber tags to follow QR codes

--- infer.sh
+++ infer.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 . torch/bin/activate
-exec nice -n +20 ./infer.py $*
+nice -n +20 ./infer.py $*
+# shell copy additional tags
+# avoid adding code to complicate infer.py source
+for filename in $*
+do
+filename_without_extension="${filename%.*}"
+extension="${filename##*.}"
+exiftool -quiet -overwrite_original_in_place -TagsFromFile ${filename} -exif:ImageNumber ${filename_without_extension}-seg.png
+done
--- mark.sh
+++ mark.sh
@@ -7,5 +7,6 @@ extension="${filename##*.}"
 # echo $filenoext $extension
 ../en480img/en480img.py $filename
 sh /tmp/en480marker.sh $filename ${filename_without_extension}-mark.$extension
+exiftool -quiet -overwrite_original_in_place -TagsFromFile ${filename} -exif:ImageNumber ${filename_without_extension}-mark.$extension
 echo Output: ${filename_without_extension}-mark.$extension
-done;
+done
